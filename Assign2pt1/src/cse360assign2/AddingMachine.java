e7e13e812df57d3ecc9480d055b7f449c16ff5ea
gannonclark <gannonclark@10.0.0.19>
2019-10-09 21:58:35 -0700

    Initial Version

---
+++ Assign2pt1/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,54 @@
+package cse360assign2;
+
+/**
+* @author Gannonclark
+* ClassId: 70641
+* Description: program tracks add and subtract transactions and prints accordingly
+* Assignment: 2
+*/
+
+public class AddingMachine {
+ 
+
+	private int total;
+	
+  public AddingMachine() {
+      total = 0;  // not needed - included for clarity
+  }
+
+
+  /**
+   * @return total
+   */
+  public int getTotal() {
+      return 0;
+  }
+
+  /**
+   * @param value to add
+   */
+  public void add(int value) {
+      
+  }
+
+  /**
+   * @param value to subtract
+   */
+
+  public void subtract(int value) {
+      
+  }
+
+  /**
+   * @return transactions of operation done
+   */
+
+  public String toString() {
+      return "";
+  }
+
+  public void clear() {
+     
+
+  }
+}
